86650f5aebc51ae3a0bd8d87968980806d134162
johngiangrossi <john.giangrossi@email.com>
2025-07-09 22:57:13 +0200

    ADD APP e Prodotto

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        // uso il costruttore e inserisco i parametri del prodotto
+        Prodotto prodotto1 = new Prodotto("biscotti", 10);
+        
+        // calcolo prezzo finale
+        double finalPrice = prodotto1.getFinalPrice();
 
+        // stampo prezzo finale e base
+        System.out.println("il prezzo base è: " + prodotto1.getPrice());
+        System.out.println("il prezzo finale è: " + finalPrice);
+
+        // stampo il nome completo con il codice prodotto
+        String fullName = prodotto1.getFullName();
+        System.out.println(fullName);
 
     }
 }
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,4 +1,48 @@
 public final class Prodotto {
     
+    // fields
+    public int code;
+    public String name;
+    public String description;
+    public double price;
+    public int iva;
+    
+    // costruttore
+    public Prodotto(String name, double price) {
 
+        // verifica dei parametri corretti
+        if (name == null || "".equals(name) || price <= 0) {
+            System.out.println("hai inserito valori errati");
+            this.name = "";
+            this.price = Double.NaN;
+        } else {
+            this.code = getNewProductNumber();
+            this.name = name;
+            this.price = price;
+            this.iva = 22;
+        }
+    }
+
+    // ottengo il prezzo base
+    public double getPrice() {
+        return this.price;
+    }
+    
+    // ottengo il nuovo codice prodotto
+    public int getNewProductNumber() {
+        int newProductNumber = (int) (Math.random() * 101);
+        return newProductNumber;
+    }
+
+    // ottengo il prezzo finale con iva
+    public double getFinalPrice() {
+        double finalPrice = this.price + ((this.price * this.iva) / 100);
+        return finalPrice;
+    }
+    
+    // ottengo il nome completo con il codice
+    public String getFullName() {
+        String fullName = "il prodotto " + this.name + " con il codice " + this.code;
+        return fullName;
+    }
 }
